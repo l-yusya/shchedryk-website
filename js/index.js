dc804e4478c2ed988221cf81de114f49be908968
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-06-17 23:01:57 +0300

    Links edited on banner

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var myNav = document.getElementById('navigation');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
