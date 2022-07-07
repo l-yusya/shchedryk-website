5f4c65164ceb41e2876e6a305e46eab299aff903
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-07-07 22:36:24 +0300

    Scroll-to-top button added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,40 @@
-var myNav = document.getElementById('navigation');
-window.onscroll = function () { 
-    "use strict";
-    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } 
-    else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
+(function () {
+  //   const pageNav = document.getElementById("navigation");
+  //   window.onscroll = function () {
+  //     "use strict";
+  //     if (
+  //       document.body.scrollTop >= 200 ||
+  //       document.documentElement.scrollTop >= 200
+  //     ) {
+  //       pageNav.classList.add("nav-colored");
+  //       pageNav.classList.remove("nav-transparent");
+  //     } else {
+  //       pageNav.classList.add("nav-transparent");
+  //       pageNav.classList.remove("nav-colored");
+  //     }
+  //   };
+
+  const scrollButton = document.getElementById("scrollBtn");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 400 ||
+      document.documentElement.scrollTop > 400
+    ) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
     }
-};
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  scrollButton.addEventListener("click", topFunction);
+})();
