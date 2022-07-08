9128976ebe882e10669a658668e5da3059d5df4c
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-07-08 14:07:01 +0300

    Sticky menu created + minor changes

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -10,12 +10,32 @@
   const navLinks = document.querySelectorAll(".navigation__link");
   navLinks.forEach((navLink) => {
     navLink.addEventListener("click", (_) => {
-          if (iconBurger.classList.contains("active")) {
-              document.body.classList.remove("lock");
-              iconBurger.classList.remove("active");
-              navigationsBody.classList.remove("active");
-          }
-        }
-    );
+      if (iconBurger.classList.contains("active")) {
+        document.body.classList.remove("lock");
+        iconBurger.classList.remove("active");
+        navigationsBody.classList.remove("active");
+      }
+    });
+  });
+
+  const iconBurgerSticky = document.querySelector(".navigation-burger-sticky");
+  const navigationsBodySticky = document.querySelector(
+    ".navigation-wrapper-sticky"
+  );
+  iconBurgerSticky.addEventListener("click", function (e) {
+    document.body.classList.toggle("lock");
+    iconBurgerSticky.classList.toggle("active");
+    navigationsBodySticky.classList.toggle("active");
+  });
+
+  const navLinksSticky = document.querySelectorAll(".navigation__link-sticky");
+  navLinksSticky.forEach((navLinkSticky) => {
+    navLinkSticky.addEventListener("click", (_) => {
+      if (iconBurgerSticky.classList.contains("active")) {
+        document.body.classList.remove("lock");
+        iconBurgerSticky.classList.remove("active");
+        navigationsBodySticky.classList.remove("active");
+      }
+    });
   });
 })();
--- js/index.js
+++ js/index.js
@@ -1,23 +1,9 @@
 (function () {
-  //   const pageNav = document.getElementById("navigation");
-  //   window.onscroll = function () {
-  //     "use strict";
-  //     if (
-  //       document.body.scrollTop >= 200 ||
-  //       document.documentElement.scrollTop >= 200
-  //     ) {
-  //       pageNav.classList.add("nav-colored");
-  //       pageNav.classList.remove("nav-transparent");
-  //     } else {
-  //       pageNav.classList.add("nav-transparent");
-  //       pageNav.classList.remove("nav-colored");
-  //     }
-  //   };
-
   const scrollButton = document.getElementById("scrollBtn");
 
   window.onscroll = function () {
     scrollFunction();
+    scrollFunctionMenu();
   };
 
   function scrollFunction() {
@@ -37,4 +23,17 @@
   }
 
   scrollButton.addEventListener("click", topFunction);
+
+  const stickyMenu = document.getElementById("sticky__menu");
+
+  function scrollFunctionMenu() {
+    if (
+      document.body.scrollTop > 180 ||
+      document.documentElement.scrollTop > 180
+    ) {
+      stickyMenu.style.display = "flex";
+    } else {
+      stickyMenu.style.display = "none";
+    }
+  }
 })();
