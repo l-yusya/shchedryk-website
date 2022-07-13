e9328a9dedbfae16abf5b7c723e71d7e462df704
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-07-13 19:07:25 +0300

    Working on the video section

--- js/media.js
+++ js/media.js
@@ -1,63 +1,26 @@
-// const acc = document.querySelectorAll(".discography__acc-button");
-// let i;
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     let panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-// const acc = document.getElementsByClassName("accordion");
-// let i;
+(function () {
+  $(".accordion").click(function () {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).next().removeClass("show");
+    } else {
+      $(".accordion").removeClass("active");
+      $(".panel").removeClass("show");
 
-// function click_action() {
-//   $(".panel").removeClass("show");
-//   $(".accordion").removeClass("active");
-//   this.classList.toggle("active");
-//   this.nextElementSibling.classList.toggle("show");
-// }
+      $(this).addClass("active");
+      $(this).next().addClass("show");
+    }
+  });
+})();
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", click_action);
-// }
+(function () {
+  const placeholder = document.querySelector(".video__placeholder--container");
+  const video = placeholder.nextElementSibling;
 
-// const acc = document.getElementsByClassName("accordion");
-// const panels = document.getElementsByClassName("panel");
-// let i;
-// let j;
+  placeholder.addEventListener("click", switchToVideo);
 
-// const handleAccordionClick = function () {
-//   for (j = 0; j < panels.length; j++) {
-//     if (this.classList.contains("active")) {
-//       this.classList.remove("active");
-//       this.nextElementSibling.classList.remove("show");
-//     } else {
-//       acc[j].classList.remove("active");
-//       panels[j].classList.remove("show");
-
-//       this.classList.add("active");
-//       this.nextElementSibling.classList.add("show");
-//     }
-//   }
-// };
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].onclick = handleAccordionClick;
-// }
-
-$(".accordion").click(function () {
-  if ($(this).hasClass("active")) {
-    $(this).removeClass("active");
-    $(this).next().removeClass("show");
-  } else {
-    $(".accordion").removeClass("active");
-    $(".panel").removeClass("show");
-
-    $(this).addClass("active");
-    $(this).next().addClass("show");
+  function switchToVideo() {
+    placeholder.style.display = "none";
+    video.style.display = "block";
   }
-});
+})();
