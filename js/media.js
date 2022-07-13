41a4f3c4e50714df88142e5e4ea0edf270e05936
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-07-13 15:46:30 +0300

    Accordeon on Media page styled and programmed
    Need to add music files

--- js/media.js
+++ js/media.js
@@ -1,13 +1,63 @@
-const acc = document.getElementsByClassName("discography__acc-button");
-let i;
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    let panel = this.parentElement.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+// const acc = document.querySelectorAll(".discography__acc-button");
+// let i;
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//     let panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+// const acc = document.getElementsByClassName("accordion");
+// let i;
+
+// function click_action() {
+//   $(".panel").removeClass("show");
+//   $(".accordion").removeClass("active");
+//   this.classList.toggle("active");
+//   this.nextElementSibling.classList.toggle("show");
+// }
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", click_action);
+// }
+
+// const acc = document.getElementsByClassName("accordion");
+// const panels = document.getElementsByClassName("panel");
+// let i;
+// let j;
+
+// const handleAccordionClick = function () {
+//   for (j = 0; j < panels.length; j++) {
+//     if (this.classList.contains("active")) {
+//       this.classList.remove("active");
+//       this.nextElementSibling.classList.remove("show");
+//     } else {
+//       acc[j].classList.remove("active");
+//       panels[j].classList.remove("show");
+
+//       this.classList.add("active");
+//       this.nextElementSibling.classList.add("show");
+//     }
+//   }
+// };
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].onclick = handleAccordionClick;
+// }
+
+$(".accordion").click(function () {
+  if ($(this).hasClass("active")) {
+    $(this).removeClass("active");
+    $(this).next().removeClass("show");
+  } else {
+    $(".accordion").removeClass("active");
+    $(".panel").removeClass("show");
+
+    $(this).addClass("active");
+    $(this).next().addClass("show");
+  }
+});
