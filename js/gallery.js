14a503eeda70c0d2a12549b814cf6618848f34ac
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-07-21 15:17:13 +0300

    Final changes before translation
    Links in forms changed to A.B., captions to pics in gallery added.

--- js/gallery.js
+++ js/gallery.js
@@ -4,77 +4,152 @@
         <img src="img/gallery/foto-01.jpg" 
             alt=""
                 class="gallery__img--main">
+        <p class="gallery__img--text">
+         Німеччина, Бонн. Виступ на Міжнародному 
+         музичному фестивалі «Бетховен фест». 2010 р.
+         </p>     
     </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-02.jpg" 
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+        <p class="gallery__img--text">
+         Німеччина, Бонн. Концертний зал Арітмеум. 
+         Святковий концерт «Concerto discretto».  
+         2007 р.
+         </p>          
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-03.jpg" 
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         США, Чикаго. Концертне турне містами США. 1994 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-04.jpg" 
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Україна, Київ. Національна філармонія. Колонна зала 
+         ім. Лисенко. Ювілейний концерт з нагоди 10-річчя хору. 
+         1981 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-05.jpg" 
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Україна, Ворзель. Піонерський табір «Светлый луч». 
+         Першій виїзд хору на літній відпочинок. 1972 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-06.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p id="gallery__img--text-small" class="gallery__img--text">
+         Німеччина, Кронберг. Berliner Platz. Виступ з 
+         нагоди вшанування пам’яті М. Ростроповича 
+         «Зустріч зі Славою». Урочиста промова Директора 
+         Rostropovich cello foundation, Kronberg Academy 
+         Раймунда Тренклєра. 2011 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-07.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Україна, Київ. Національна філармонія. 
+         Колонна зала ім. Лисенко. Прем’єрне виконання 
+         в Україні твору Г.Канчелі «Янголи печалі». 
+         Відкрита репетиція. 2015 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-08.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Україна, Київ. Лютеранська церква св. Катерини. 
+         Різдвяний концерт за участі молодшого складу хору. 
+         2014 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-09.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Україна, Київ. Національна філармонія. Колонна зала 
+         ім. Лисенко. Ювілейний концерт з нагоди 40-річчя хору. 
+         2011 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-10.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Німеччина, Кронберг. Музична академія 
+         ім. М. Ростроповича
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-11.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Німеччина, Кронберг. Johanniskirche Kronberg. 
+         Концертний виступ на Міжнародному фестивалі 
+         камерної музики «Kronberg academy Festival». 
+         2013 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-12.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Ватикан. Собор Святого Павла. Конкурсний виступ 
+         на Міжнародному хоровому конкурсі «Musica Sacra a Roma». 
+         2005 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-13.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Виступ хору Щедрик в церкві святого 
+         Бартоломея у Гданську. 2017 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-14.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+                        <p class="gallery__img--text">
+         Німеччина, Берлін. Філармонія Берліну. 
+         Камерна музична сцена. Концерт у підтримку 
+         політичних в’язнів Росії «To Russia 
+         with love». 2013 р.
+         </p> 
+    </div>`,
     `<div class="carousel__item">
         <img src="img/gallery/foto-16.jpg"
             alt=""
                 class="gallery__img--main">
-                            </div>`,
+        <p class="gallery__img--text">
+         Австрія, Локенхуаз. Lockenhaus Kirche. 
+         Після виступу на Міжнародному музичному 
+         фестивалі «Kammermusikfest Lockenhaus». 
+         2008 р.
+         </p> 
+    </div>`,
   ];
 
   let slideIdx = 0;
